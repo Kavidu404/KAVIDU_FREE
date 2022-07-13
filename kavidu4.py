bb7bc654030e44ea6aa927189d1821f206ff5819
ωнαιт●⸽⸽✇⸽⸽●нα¢αєя <90969493+Kavidu404@users.noreply.github.com>
2022-07-13 12:16:20 +0100

    .

--- kavidu4.py
+++ kavidu4.py
@@ -10,6 +10,6 @@ except KeyError:
 if __name__ == "__main__":
 	os.system("git pull")
 	if "Nigeria" == fc:
-		__import__("kavidu4").login()
+		__import__("kavidu4").main()
 	else:
-		__import__("kavidu4").login()
+		__import__("kavidu4").main()
